4dd2396a82b05ab08f4beb61f9934e3fbb186168
dovydasvg <dovydasvg@yahoo.fr>
2021-04-26 14:48:24 +0300

    Added Answer model

---
+++ database/factories/AnswerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Answer;
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AnswerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Answer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'rating' => $this->faker->numberBetween(1,5),
+            'question_id' => Question::all()->random(1)->first()->id,
+            'user_id' => User::all()->random(1)->first()->id,
+        ];
+    }
+}
