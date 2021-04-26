1d7faed0b08ef5f9cae05bb3b3e3a735027a7e1d
dovydasvg <dovydasvg@yahoo.fr>
2021-04-26 15:34:40 +0300

    Create a question component

---
+++ resources/js/components/Question.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h1 class="flex justify-center font-bold">{{content}}</h1>
+                <div class="flex justify-center">
+                    <label class="my-2 text-center">1<br>not at all</label>
+                <input class="m-2" type="radio" id="1" :name="question_id" value="1">
+                <input class="m-2" type="radio" id="2" :name="question_id" value="2">
+                <input class="m-2" type="radio" id="3" :name="question_id" value="3">
+                <input class="m-2" type="radio" id="4" :name="question_id" value="4">
+                <input class="m-2" type="radio" id="5" :name="question_id" value="5">
+                    <label class="my-2 text-center">5<br>a lot</label>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Question",
+    props:[
+        'content',
+        'question_id'
+    ]
+}
+</script>
+
+<style scoped>
+
+</style>
