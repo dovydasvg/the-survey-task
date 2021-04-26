80072043c4a00ebd4921eb10e1c882c47d0b4ab8
dovydasvg <dovydasvg@yahoo.fr>
2021-04-26 14:51:35 +0300

    Added AnswerSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-        $this->call(
-            QuestionSeeder::class
-        );
+        $this->call([
+            QuestionSeeder::class,
+            AnswerSeeder::class
+        ]);
     }
 }
