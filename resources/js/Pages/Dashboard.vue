a57e9ae5f0a7cf919364dd5469058638993d3494
dovydasvg <dovydasvg@yahoo.fr>
2021-04-27 10:18:43 +0300

    Added Question component

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,28 +2,50 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                The Survey
             </h2>
         </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
-                </div>
-            </div>
-        </div>
+        <form @submit.prevent="submit">
+        <question @answered="saveAnswer" v-for="question in questions.data" :content="question.content" :question_id="question.id"></question>
+        <button type="button" v-if="questions.current_page !== questions.last_page" @click="nextPage">Next</button>
+        </form>
+        {{questions}}
     </app-layout>
 </template>
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
     import Welcome from '@/Jetstream/Welcome'
+    import Question from '@/components/Question'
 
     export default {
         components: {
             AppLayout,
             Welcome,
+            Question
         },
+        props: [
+            'questions',
+        ],
+        data: function () {
+            return{
+                //Keys: Question ID; Value: Answer;
+                form: {}
+            }
+        },
+        methods:{
+            submit()
+            {
+                console.log(this.form)
+            },
+            saveAnswer(val)
+            {
+                this.form[val.question_id] = val.value
+            },
+            newPage()
+            {
+
+            }
+        }
     }
 </script>
