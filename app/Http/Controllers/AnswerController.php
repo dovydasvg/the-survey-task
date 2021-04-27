e26818fbef27cf6c99f3e42752a7be1d798b5c2e
dovydasvg <dovydasvg@yahoo.fr>
2021-04-27 14:22:12 +0300

    added store method

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Answer;
 
 class AnswerController extends Controller
 {
+    public function store(Request $request)
+    {
+        $current_user = auth()->user()->id;
+
+        foreach($request->all() as $answer)
+        {
+            //Check if an Answer already exists. Update if yes, create new if not.
+            if(Answer::where('user_id','=',$current_user)->where('question_id','=',$answer['question_id'])->exists())
+            {
+                $newAnswer = Answer::where('user_id','=',$current_user)
+                    ->where('question_id','=',$answer['question_id'])
+                    ->update(['rating'=>$answer['rating']]);
+            }
+            else {
+                Answer::create([
+                        'question_id' => $answer['question_id'],
+                        'rating' => $answer['rating'],
+                        'user_id' => $current_user,
+                    ]
+                );
+            }
+        }
+    }
 
 }
