f18e6851299a738a5cb7daaed9934c107f40ed17
dovydasvg <dovydasvg@yahoo.fr>
2021-04-27 10:19:45 +0300

    Added index method

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class QuestionController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Dashboard',
+        [
+           'questions' => Question::Paginate(10),
+        ]);
+    }
 }
