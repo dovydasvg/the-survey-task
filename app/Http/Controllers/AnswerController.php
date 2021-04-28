26f4bd8a2e00113ea0f9473e6bc2b953d72a205f
dovydasvg <dovydasvg@yahoo.fr>
2021-04-28 09:19:12 +0300

    Rerouting added and results method

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Answer;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+
 
 class AnswerController extends Controller
 {
     public function store(Request $request)
     {
         $current_user = auth()->user()->id;
+        $redirect_url = '';
 
         foreach($request->all() as $answer)
         {
+            //Each element has a next_page value
+            $redirect_url = $answer['next_page'];
             //Check if an Answer already exists. Update if yes, create new if not.
             if(Answer::where('user_id','=',$current_user)->where('question_id','=',$answer['question_id'])->exists())
             {
@@ -29,6 +35,29 @@ class AnswerController extends Controller
                 );
             }
         }
+
+        return Inertia::location($redirect_url);
+
+
+    }
+
+    public function results()
+    {
+        $results = [];
+        foreach(Answer::where('user_id','=', auth()->user()->id)->get() as $answer)
+        {
+            array_push($results,
+            [
+                'question' => $answer->question()->get()->first()->content,
+                'user_rating' => $answer->rating,
+                'average_rating' => $answer->question()->get()->first()->average()
+            ]);
+        }
+
+        return Inertia::render('Results',
+            [
+                'results' => $results
+            ]);
     }
 
 }
