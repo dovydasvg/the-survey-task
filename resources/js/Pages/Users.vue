997ab7a4e99f670ee76d1450b08e7928dc2aa27a
dovydasvg <dovydasvg@yahoo.fr>
2021-04-28 11:09:28 +0300

    Create users view

---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,34 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Users
+            </h2>
+        </template>
+        <ul>
+            <user v-for="user in users">{{user.email}}</user>
+        </ul>
+
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import User from "@/components/User";
+
+export default {
+    name: "Users",
+    //Getting only users that didn't finish the survey
+    props:[
+        'users',
+    ],
+    components:{
+        AppLayout,
+        User
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
