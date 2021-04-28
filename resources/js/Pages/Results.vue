047d64fe0c707b0528ce94c977f5d5bbfd02ec95
dovydasvg <dovydasvg@yahoo.fr>
2021-04-28 09:16:53 +0300

    Created results page and component

---
+++ resources/js/Pages/Results.vue
@@ -0,0 +1,32 @@
+<template>
+<app-layout>
+    <template #header>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Results
+        </h2>
+    </template>
+    <div class="grid grid-cols-1 justify-items-center">
+    <ResultGraph v-for="result in results" :id="result.question" :question="result.question" :user_rating="result.user_rating" :average_rating="result.average_rating" class="w-1/2"></ResultGraph>
+    </div>
+
+</app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import ResultGraph from "@/components/ResultGraph";
+
+export default {
+    name: "Results",
+    components: {ResultGraph, AppLayout},
+    props:{
+        results:[]
+    }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ resources/js/components/ResultGraph.vue
@@ -0,0 +1,42 @@
+<script>
+import { Bar } from 'vue3-chart-v2'
+
+export default {
+    name: "ResultGraph",
+    extends: Bar,
+    props: [
+        'question',
+        'user_rating',
+        'average_rating'
+    ],
+    mounted () {
+        this.renderChart({
+            labels: ['Your Answer', 'Average Answer'],
+            datasets: [
+                {
+                    label: this.question,
+                    backgroundColor: '#f87979',
+                    data: [this.user_rating, this.average_rating],
+
+                }
+            ]
+
+        }, {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        suggestedMax:5,
+                        stepSize: 1,
+                        precision: 5
+                    }
+                }]
+            }
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
