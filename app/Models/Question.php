29b7056b1eed16fe82101817e3994052a5e0a291
dovydasvg <dovydasvg@yahoo.fr>
2021-04-28 09:17:52 +0300

    Added average method

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,15 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Answer;
 
 class Question extends Model
 {
     use HasFactory;
 
-    protected $fillable=['content','average'];
+    protected $fillable = ['content', 'average'];
 
     public function answers()
     {
         return $this->hasMany(Answer::class);
+    }
+
+    public function average()
+    {
+        return Answer::where('question_id','=', $this->id)->avg('rating');
     }
 }
