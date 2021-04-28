02b82a933faef462de21e0afcc65b3d1a0d872f8
dovydasvg <dovydasvg@yahoo.fr>
2021-04-28 11:10:06 +0300

    create user component

---
+++ resources/js/components/User.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg text-center p-4">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "User"
+}
+</script>
+
+<style scoped>
+
+</style>
